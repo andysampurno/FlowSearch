d4a9dd5631753483ea39282313362489ed382e61
Andy Sampurno <m.andysampurno@gmail.com>
2020-06-08 08:09:25 +0700

    Progres 1

---
+++ app/src/main/java/com/jc/flowsearch/MainActivity.java
@@ -0,0 +1,30 @@
+package com.jc.flowsearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnCamera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCamera = (Button)findViewById(R.id.qBtnCamera);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, camera.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
